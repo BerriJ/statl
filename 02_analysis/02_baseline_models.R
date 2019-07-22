e5984ae06dd4781c6c6bf3bbd1d1a74561cd5089
Jonathan <jonathan@berrisch.biz>
2019-07-22 12:43:16 +0200

    Now computing out of sample means in 02_baseline

--- 02_analysis/02_baseline_models.R
+++ 02_analysis/02_baseline_models.R
@@ -1,12 +1,19 @@
 load("00_data/wine_preprocessed.rda")
 
+train <- sample(nrow(wine), floor(0.75*nrow(wine)))
+
+wine_train <- wine[train,]
+wine_test <- wine[-(train),]
+
+
 # Baseline Model: Mean
 
 models <- data.frame(mod = c("Mean", "Basic_lm"), rmse = c(NA, NA))
 
-residuals <- wine$llitre-mean(wine$llitre, na.rm = T) %>% na.omit()
+residuals <- wine_train$llitre-mean(wine_train$llitre, na.rm = T) %>% na.omit()
 plot(residuals)
-models[models$mod == "Mean","rmse"] <- (wine$llitre-mean(wine$llitre, na.rm = T))^2 %>% 
+
+models[models$mod == "Mean","rmse"] <- (wine_test$llitre-mean(wine$llitre, na.rm = T))^2 %>% 
   na.omit() %>%
   sqrt() %>%
   mean()
@@ -14,15 +21,19 @@ print(models)
 
 # # Baseline Model: Linear Model with all* variables
 
-colSums(is.na(wine)) %>% sort(decreasing = T) # We've got some problems
+colSums(is.na(wine_train)) %>% sort(decreasing = T) # We've got some problems
 
-wine_subset <- wine[,which(colSums(is.na(wine)) < 1000)] %>% 
-  drop_na()
+wine_subset <- wine_train[,which(colSums(is.na(wine)) < 1000)] %>% 
+  drop_na() %>% dplyr::select(-name, - litre)
 
-lmod <- lm(llitre ~. -litre -name, data = wine_subset)
+lmod <- lm(llitre ~., data = wine_subset)
 
-models[models$mod == "Basic_lm", "rmse"] <- (wine_subset$llitre-lmod$fitted.values)^2 %>% 
+preds <- predict(lmod, newdata = wine_test)
+
+models[models$mod == "Basic_lm", "rmse"] <- (na.omit(wine_test$llitre-preds))^2 %>% 
   sqrt() %>%
   mean()
 
 models
+
+
