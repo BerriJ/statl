776bb824026eda92e3dee692a119a2ad341c5fa7
Timo <t.rammert03@gmail.com>
2019-07-30 15:18:11 +0200

    Update 04_log_question.R

--- 04_log_question.R
+++ 04_log_question.R
@@ -9,3 +9,23 @@ plot(Wein_train$litre)
 plot(Wein_train$llitre)
 
 plot(log(Wein_train$llitre))
+
+
+# Der Einfachalthalber habe ich nur die ersten n Beobachtungen betrachtet
+
+plot(Wein_train$litre[1:100])
+
+# Warum sind die folgenden beiden nicht identisch?
+par(mfrow= c(2,1))
+
+plot(Wein_train$llitre[1:100],ylim = c(1000000,10000000))
+
+plot(log(as.numeric(Wein_train$litre[1:100]))*1000000, ylim = c(1000000,10000000))
+
+diff <- (Wein_train$llitre-log(as.numeric(Wein_train$litre))*1000000)[1:100]
+
+diff # Abgesehen von einigen Ausreißern (besonders niedrige Werte) (siehe Grafiken) gute Näherung
+
+par(mfrow= c(1,1))
+hist(diff, freq = F)
+
