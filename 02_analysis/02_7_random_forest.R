bfc7668cc6c77fe46634f0974ddac78b7e350e10
Jonathan <jonathan@berrisch.biz>
2019-08-23 20:03:58 +0200

    Revert "Attempt to improve random forest performance"
    This reverts commit 00ea7f0afb89f870b7f60135457faa6dde700f11.

--- 02_analysis/02_7_random_forest.R
+++ 02_analysis/02_7_random_forest.R
@@ -10,26 +10,21 @@ print("Now doing the Random Forest-Loop")
 
 m <- seq(10,100,10)
 trees <- c(1,3,5,10,15,20,25)
-grid <- expand.grid(m,trees) %>% arrange(Var1, Var2)
-grid <- data.frame(grid, fold = rep(NA, nrow(grid)))
-colnames(grid) <- c("mtry","trees", paste("rmse_fold_",i,sep = ""))
-z <- 0
+grid <- expand.grid(m,trees)
+rmse_RF <- c()
 d <- c()
 tic()
-for(k in 1:length(m)){
+for(i in 1:nrow(grid)){
   x <- Sys.time()
   rf_wine <- randomForest(x = x.train, y = y.train, importance = T,
-                          mtry = m[k], ntree = max(grid$trees))
-  for(t in 1:length(trees)){
-    z <- z+1
-    pred_rf <- predict(rf_wine, newdata = x.test, n.trees = trees[t])
-    grid[z,3] <- mean((y.test - pred_rf)^2) %>% sqrt()
-  }
+                          mtry = grid[i,1], ntree = grid[i,2])
+  pred_rf <- predict(rf_wine, newdata = x.test, n.trees = grid[i,2])
+  rmse_RF[i] <- mean((y.test - pred_rf)^2) %>% sqrt()
   d[i] <- Sys.time() - x
-  print(paste("Iteration No. ",k, " ~ ",round(mean(d)*(nrow(grid)-k)), " minutes remaining.", sep = ""))
+  print(paste("Iteration No. ",i, " ~ ",round(mean(d)*(nrow(grid)-i)), " minutes remaining.", sep = ""))
 }
 toc()
 
 dir.create("02_analysis/cv/rf/", recursive = T, showWarnings = F)
 save(file = paste("02_analysis/cv/rf/rf_",unique_identifier,".rda", sep = ""),
-     grid)
+     grid, rmse_RF)
