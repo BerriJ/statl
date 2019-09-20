87b508c33065014e466ba90578cb585e96dd680d
Jonathan <jonathan@berrisch.biz>
2019-09-20 08:10:03 +0200

    Update lasso plot

--- 04_presentation/ggplot.lasso.R
+++ 04_presentation/ggplot.lasso.R
@@ -2,12 +2,12 @@ library(reshape2)
 
 beta=coef(mod)
 tmp <- as.data.frame(as.matrix(beta))
+tmp <- tmp[c(rowMeans(tmp) >= 1000),]
 tmp$coef <- row.names(tmp)
 tmp <- reshape2::melt(tmp, id = "coef")
 tmp$variable <- as.numeric(gsub("s", "", tmp$variable))
 tmp$lambda <- mod$lambda[tmp$variable+1] # extract the lambda values
 tmp$norm <- apply(abs(beta[-1,]), 2, sum)[tmp$variable+1] # compute L1 norm
-
 tmp_alt <- tmp[tmp$value !=0,]
 
 lasso.plot <- ggplot(tmp[tmp$coef != "(Intercept)",], aes(log(lambda), value, group = coef)) + 
